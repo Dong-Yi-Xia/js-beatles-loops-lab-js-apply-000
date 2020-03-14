f98b17dcd88afbd21955580cee6c63e711a4cca3
Dong Xia <dong7777xia@gmail.com>
2020-03-14 15:03:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // add solution here
 function theBeatlesPlay(musicians, instruments){
   let phrase = []
+  for (let i = 0; i < musicians.length; i++){
+    let str = `${musicians[i]} plays ${instruments[i]}`
+  }
 }
