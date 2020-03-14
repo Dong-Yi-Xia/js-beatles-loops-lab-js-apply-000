4cea44102afb92d02a047ac8530de2f915de63ac
Dong Xia <dong7777xia@gmail.com>
2020-03-14 15:05:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   let phrase = []
   for (let i = 0; i < musicians.length; i++){
     let str = `${musicians[i]} plays ${instruments[i]}`
+    phrase.push(str)
   }
+  return phrase
 }
